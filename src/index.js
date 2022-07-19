bb08e247ef8bcb8a494c3bfb0ebe49e6fb9a8624
Adele_D <elmaadeledube@gmail.com>
2022-07-19 13:13:41 +0300

    Made API call to get current weather and populated JS file with the response
    Weather data collected from https://openweathermap.org/current

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
-let apiKey = "1a915758c5fb84c9ee7377f6039e76a7l"
+function displayTemp(response) {
+  console.log(response.data);
+  console.log(response.data.main.temp);
 
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+  //#today div
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.name;
+
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let windSpeedElement = document.querySelector("#windspeed");
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed) + " km/h";
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+
+  //Date-Time
+  let now = new Date();
+
+  let timeElement = document.querySelector("#current-date");
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+  ];
+
+  let day = days[now.getDay()];
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  timeElement.innerHTML = `${day}, ${date}/${month}/${year} </br> Local time ${hours}:${minutes}`;
+
+  //
+}
+
+apiKey = "1a915758c5fb84c9ee7377f6039e76a7";
+let city = "Swakopmund";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemp);
